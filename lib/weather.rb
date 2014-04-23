8e36220065a666c2203efdcf1dad9ab4f3510683
Robert Leon <llexi@hotmail.com>
2014-04-23 23:51:03 +0100

    Changed weather state to conditions for readability

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,6 +1,6 @@
 class Weather
 
-  def state
+  def conditions
     return :sunny if Random.rand(0..1) == 0
     :stormy
   end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -9,13 +9,13 @@ describe Weather do
     it 'will be sunny half the time' do
       expect(Random).to receive(:rand).with(0..1).and_return(0)
 
-      expect(weather.state).to eq :sunny
+      expect(weather.conditions).to eq :sunny
     end
 
     it 'will be stormy half the time' do
       expect(Random).to receive(:rand).with(0..1).and_return(1)
 
-      expect(weather.state).to eq :stormy
+      expect(weather.conditions).to eq :stormy
     end
 
   end
